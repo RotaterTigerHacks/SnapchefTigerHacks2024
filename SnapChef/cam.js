481db1624dd912777b74865c60e24ccc820966b6
etsm2n <etsm2n@umsystem.edu>
2024-11-01 22:52:44 -0500

    Update cam.js

--- SnapChef/cam.js
+++ SnapChef/cam.js
@@ -4,7 +4,7 @@
     let streaming = false;
 
     const video = document.getElementById('video');
-    const canvas = document.getElementById('Mycanvas');
+    const canvas = document.getElementById('myCanvas');
     const photo = document.getElementById('photo');
     const startbutton = document.getElementById('startbutton');
 
