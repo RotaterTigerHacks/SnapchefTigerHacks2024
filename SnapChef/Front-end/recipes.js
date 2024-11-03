155832d32993413d75b85080810fb4e5b70c58c8
etsm2n <etsm2n@umsystem.edu>
2024-11-02 23:20:16 -0500

    Update recipes.js

--- SnapChef/Front-end/recipes.js
+++ SnapChef/Front-end/recipes.js
@@ -1,3 +1,5 @@
-var image1 = document.createElement("image");
+var image1 = document.getElementById("recipe1");
 var image2 = document.createElement("image");
 var image3 = document.createElement("image");
+
+image1.src = "https://th.bing.com/th/id/R.a2ab10f92d9ca02c5528421014d53875?rik=NDb6ah5tNLQz1g&riu=http%3a%2f%2fapple.gop%2fmedia%2fuploads%2fzinnia%2fRubens_apples_on_plate.jpg&ehk=GYD%2bB0IezSB1Af2rLdd%2bXS9AEbaO36h5J0JTEQmiHpc%3d&risl=&pid=ImgRaw&r=0";
